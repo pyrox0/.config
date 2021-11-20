a7c5f58c9d278b55500620520ff3fb387f5d4a96
Modded Gamers <hedgehog@mrhedgehog.xyz>
2021-11-20 16:37:03 -0500

    Update wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,5 +1,5 @@
 local fish;
-local wezterm = require'wezterm'
+local wezterm = require'wezterm';
 
 if  os.getenv("__CFBundleIdentifier") then
     fish = "/Users/993390/.brew/bin/fish"
@@ -7,10 +7,67 @@ else
     fish = "/usr/bin/fish"
 end
 
+local BOX = utf8.char(0x275A)
+local BAR = utf8.char(0x2758)
+
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+  local edge_background = "#FB0022"
+  local background = "#FB1032"
+  local foreground = "#808080"
+
+  if tab.is_active then
+    background = "#2B2042"
+    foreground = "#C0C0C0"
+  elseif hover then
+    background = "#3B3052"
+    foreground = "#909090"
+  end
+
+  local edge_foreground = background
+
+  -- ensure that the titles fit in the available space,
+  -- and that we have room for the edges
+  local title = wezterm.truncate_to_width(tab.active_pane.title, max_width-2)
+
+  return {
+    {Background={Color=edge_background}},
+    {Foreground={Color=edge_foreground}},
+    {Text=BAR},
+    {Background={Color=background}},
+    {Foreground={Color=foreground}},
+    {Text=title},
+    {Background={Color=edge_background}},
+    {Foreground={Color=edge_foreground}},
+    {Text=BOX},
+  }
+end)
+
 return {
+	-- Launch fish by default
     default_prog = {fish, "-l"},
+
+	-- Set TERM to wezterm
+	-- This is installed on all my machines
     term = "wezterm",
    
-	-- Set font
-    font = wezterm.font("FiraCode Nerd Font Mono")
+	-- Set fonts
+	font_size = 13,
+    font = wezterm.font_with_fallback({
+		"Input Mono",
+		"FuraCode Nerd Font Mono",
+		"all-the-icons",
+		"file-icons",
+		"FontAwesome",
+		"github-octicons",
+		"Material Icons",
+		"Weather Icons"
+	}),
+
+	-- Set color scheme
+	color_scheme = "Dracula",
+	
+	-- We want to use our fonts for everything
+	custom_block_glyphs = false,
+
+
 }
