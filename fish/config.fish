faca95c955fc7c4b3d90181f146966941a72b575
Modded Gamers <hedgehog@mrhedgehog.xyz>
2021-11-20 16:38:14 -0500

    Update fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,10 @@
 if status is-interactive
     starship init fish | source
     zoxide init --cmd cd fish | source
-	source "$__fish_config_dir"/gpg.fish
 end
 
 source "$__fish_config_dir"/set_env.fish
 source "$__fish_config_dir"/set_path.fish
+source "$__fish_config_dir"/gpg.fish
+
+fnm env | source
--- fish/gpg.fish
+++ fish/gpg.fish
@@ -1,3 +1,7 @@
 set -x GPG_TTY (tty)
 set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
 gpgconf --launch gpg-agent
+pkill pinentry
+pkill gpg
+pkill ssh-agent
+eval (gpg-agent --daemon --enable-ssh-support --pinentry-program ~/.brew/bin/pinentry-mac | sed 's/=/ /g' | sed 's/SSH_AUTH_SOCK/set SSH_AUTH_SOCK/')
